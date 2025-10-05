aa8ac2e58bdc7b420a2b344392169a52b43876a1
jonathanAkabuike <n01510573@humber.ca>
2025-10-05 19:12:12 -0400

    Implemented Login using Google Sign in button with Google firebase

--- app/src/main/java/ca/light/indoorair/freshness/energy/it/life/environmental/solution/LoginActivity.java
+++ app/src/main/java/ca/light/indoorair/freshness/energy/it/life/environmental/solution/LoginActivity.java
@@ -2,16 +2,25 @@ package ca.light.indoorair.freshness.energy.it.life.environmental.solution;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.content.res.Configuration;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,9 +32,16 @@ public class LoginActivity extends AppCompatActivity {
 
 
     EditText usernameEditText, passwordEditText;
-    Button loginButton;
+    Button loginButton, googleSignInButton;
     TextView signup;
     CheckBox rememberMeCheckBox;
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+    private ActivityResultLauncher<IntentSenderRequest> oneTapLauncher;
+
+
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,29 +52,71 @@ public class LoginActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.editTextTextPassword);
         loginButton = findViewById(R.id.loginbutton);
         signup = findViewById(R.id.signup);
+        googleSignInButton = findViewById(R.id.googlebutton);
         rememberMeCheckBox = findViewById(R.id.checkBox);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!validateUsername() | !validatePassword()) {
-                    return;
-                } else {
-                    checkUser();
-                }
-            }});
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Show all accounts on the device.
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                .build();
 
 
+        oneTapLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartIntentSenderForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        try {
+                            SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+                            String idToken = credential.getGoogleIdToken();
+                            if (idToken != null) {
+                                // Got an ID token from Google. Use it to authenticate
+                                // with your backend.
+                                Log.d(TAG, "Got ID token.");
+                                Toast.makeText(this, "Google Sign In Successful.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        } catch (ApiException e) {
+                            Log.w(TAG, "Sign-in failed", e);
+                            Toast.makeText(this, "Google Sign In Failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
+        googleSignInButton.setOnClickListener(v -> oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener(this, result -> {
+                    try {
+                        oneTapLauncher.launch(
+                                new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build());
+                    } catch (Exception e) {
+                        Log.e(TAG, "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+                    }
+                })
+                .addOnFailureListener(this, e -> {
+                    // No Google Accounts found. Just continue presenting the signed-out UI.
+                    Log.d(TAG, e.getLocalizedMessage());
+                }));
 
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-                startActivity(intent);
+        loginButton.setOnClickListener(v -> {
+            if (!validateUsername() | !validatePassword()) {
+                return;
             }
+            checkUser();
         });
+
+
+        signup.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
+
 
     }
 
@@ -87,7 +145,7 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void checkUser(){
+    public void checkUser() {
 
         String userUsername = usernameEditText.getText().toString().trim();
         String userPassword = passwordEditText.getText().toString().trim();
@@ -106,8 +164,8 @@ public class LoginActivity extends AppCompatActivity {
 
                     if (passwordFromDB != null && passwordFromDB.equals(userPassword)) {
                         passwordEditText.setError(null);
-                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                      startActivity(intent);
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
                     } else {
                         passwordEditText.setError(getString(R.string.invalid_credentials));
                         passwordEditText.requestFocus();
@@ -120,21 +178,8 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Log.e(TAG, "Database error: " + error.getMessage());
             }
         });
     }
-
-
-
-
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Re-set the layout when the orientation changes
-        setContentView(R.layout.login_page);
-    }
-
-
 }
