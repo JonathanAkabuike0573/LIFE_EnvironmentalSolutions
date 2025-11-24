bd17abb6cd1ccb9be893803a145ec07bbb4ef3f0
jonathanAkabuike <n01510573@humber.ca>
2025-11-23 20:08:31 -0500

    Refactored the "About Us" screen to use a `WebView` to display content from a new local HTML file (`aboutpage.html`). - Wrapped the feedback layout in a `NestedScrollView` to ensure content is scrollable on smaller screens

--- lifeenvironmentalsolution/src/main/java/ca/light/indoorair/freshness/energy/it/life/environmental/solution/AboutUsFragment.java
+++ lifeenvironmentalsolution/src/main/java/ca/light/indoorair/freshness/energy/it/life/environmental/solution/AboutUsFragment.java
@@ -1,34 +1,25 @@
 package ca.light.indoorair.freshness.energy.it.life.environmental.solution;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-// Note: Ensure your build.gradle file includes androidx.appcompat or
-// androidx.fragment dependencies.
-
-/**
- * A simple {@link Fragment} subclass for displaying the About Us screen.
- * This fragment inflates the layout and no longer sets the dynamic app version text.
- */
 public class AboutUsFragment extends Fragment {
 
-    // Default public constructor is required for Fragments
-    public AboutUsFragment() {
-        // Required empty public constructor
-    }
-
+    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    } }
+        WebView webView = view.findViewById(R.id.about_us_webview);
+        webView.loadUrl("file:///android_asset/aboutpage.html");
+
+        return view;
+    }
+}
