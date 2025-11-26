f1d053d72058e5031104b3737768cc28337d0da7
MohamedAli0760 <ma921833@gmail.com>
2025-11-26 13:23:17 -0500

    Add a BiDirectionalFragment to the project and adding in information later

---
+++ lifeenvironmentalsolution/src/main/java/ca/light/indoorair/freshness/energy/it/life/environmental/solution/BiDirectionalFragment.java
@@ -0,0 +1,22 @@
+package ca.light.indoorair.freshness.energy.it.life.environmental.solution;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class BiDirectionalFragment extends Fragment {
+
+    public BiDirectionalFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bi_directional, container, false);
+    }
+}
