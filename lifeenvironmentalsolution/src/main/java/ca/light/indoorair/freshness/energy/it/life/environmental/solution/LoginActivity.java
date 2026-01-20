a36b7bc8e0b6c4de0d7e14e9399ab06a9ef2c3ca
MohamedAli0760 <ma921833@gmail.com>
2026-01-20 15:37:23 -0500

    test out new branch

--- lifeenvironmentalsolution/src/main/java/ca/light/indoorair/freshness/energy/it/life/environmental/solution/LoginActivity.java
+++ lifeenvironmentalsolution/src/main/java/ca/light/indoorair/freshness/energy/it/life/environmental/solution/LoginActivity.java
@@ -19,6 +19,7 @@ import ca.light.indoorair.freshness.energy.it.life.environmental.solution.valida
 
 // No longer importing com.google.firebase.auth.FirebaseAuthProvider
 // No longer importing static androidx.core.app.PendingIntentCompat.getActivity
+// testing out new branch
 
 public class LoginActivity extends AppCompatActivity {
 
